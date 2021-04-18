8e51ad19a5cd67cadb5b073bcc7886230b33e652
lostincode <lostincode@posteo.de>
2021-04-18 23:40:31 +0200

    Restructured code

---
+++ src/rowindexlist.rs
@@ -0,0 +1,152 @@
+use crate::sparsemat::IndexType;
+
+// Stores indices for each row for tracking the entries in the sparse matrix data vec
+// The vec index_list contains the next positions in the list while the vec row_start
+// contains the starting position for each row
+// The position in the index_list vec is the actual index
+// Example: row_start  = [0, 2, UNSET, 4, 3]
+//          index_list = [1, UNSET, UNSET, 5, 6, UNSET, UNSET]
+//          -> First row holds two entries at position 0 and 1
+//          -> Second row only holds one entry at position 2
+//          -> Third row is empty
+//          -> Fourth row holds two entries at position 4 and 6
+//          -> Fifth row holds two entries at position 3 and 5
+//             and has been inserted before the fourth row
+// Iterating over the entries of the fourth row would be look like this:
+// row_start =  [0, 2, UNSET, 4, ...]
+//                            |
+//                            -->--
+//                                |
+// index_list = [1, UNSET, UNSET, 5, 6, UNSET, UNSET]
+//                                |       |
+//                                ---->----
+#[derive(Clone, Debug)]
+pub(crate) struct RowIndexList<I> {
+    row_start: Vec<I>,
+    index_list: Vec<I>,
+}
+
+impl<I> RowIndexList<I>
+where I: IndexType {
+    pub(crate) const UNSET: I = <I as IndexType>::MAX;
+
+    // Creates an empty index list
+    pub(crate) fn new() -> Self {
+        Self {
+            row_start: Vec::<I>::new(),
+            index_list: Vec::<I>::new(),
+        }
+    }
+
+    // Creates an index list with reserved space
+    pub(crate) fn with_capacity(cap: usize) -> Self {
+        Self {
+            row_start: Vec::<I>::with_capacity(cap),
+            index_list: Vec::<I>::with_capacity(cap),
+        }
+    }
+
+    // Returns the number of entries in the index list
+    pub(crate) fn n_entries(&self) -> usize {
+        self.index_list.len()
+    }
+
+    // Returns the number of rows
+    pub(crate) fn n_rows(&self) -> usize {
+        self.row_start.len()
+    }
+
+    // Appends a new entry for row and returns its index in array
+    pub(crate) fn push(&mut self, row: usize) -> usize {
+        if row >= self.row_start.len() {
+            self.row_start.resize(row + 1, Self::UNSET);
+        }
+        let index = I::as_indextype(self.n_entries());
+        if index == Self::UNSET {
+            panic!("Maximum number of {} entries reached", Self::UNSET);
+        }
+        self.index_list.push(Self::UNSET);
+        if self.row_start[row] == Self::UNSET {
+            // This is the first entry in this row
+            self.row_start[row] = index;
+        } else {
+            // Iterate over the list of indices
+            let mut iter = self.row_start[row].as_usize();
+            while self.index_list[iter] != Self::UNSET {
+                iter = self.index_list[iter].as_usize();
+            }
+            // Append the new index to the last entry in the list
+            self.index_list[iter] = index;
+        }
+        index.as_usize()
+    }
+
+    pub(crate) fn iter_row(&self, row: usize) -> IterRow<I> {
+        let start = if row < self.n_rows() {
+            self.row_start[row]
+        } else {
+            Self::UNSET
+        };
+        IterRow::<I> {
+            list: self,
+            pos: start,
+        }
+    }
+
+    pub(crate) fn iter(&self) -> Iter<I> {
+        Iter::<I> {
+            list: self,
+            row: 0,
+            pos: self.row_start[0],
+        }
+    }
+}
+
+// Iterator returning all the index IDs of a row
+pub(crate) struct IterRow<'a, I> {
+    list: &'a RowIndexList<I>,
+    pos: I,
+}
+
+impl<'a, I> Iterator for IterRow<'a, I>
+where I: IndexType {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos != RowIndexList::<I>::UNSET {
+            let index = self.pos.as_usize();
+            self.pos = self.list.index_list[index];
+            Some(index)
+        } else {
+            None
+        }
+    }
+}
+
+// Iterator returning the row and all the index IDs
+pub(crate) struct Iter<'a, I> {
+    list: &'a RowIndexList<I>,
+    row: usize,
+    pos: I,
+}
+
+impl<'a, I> Iterator for Iter<'a, I>
+where I: IndexType {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // Increment row if necessary
+        while self.pos == RowIndexList::<I>::UNSET
+               && self.row < (self.list.n_rows() - 1) {
+            self.row += 1;
+            self.pos = self.list.row_start[self.row];
+        }
+        if self.pos != RowIndexList::<I>::UNSET {
+            let index = self.pos.as_usize();
+            self.pos = self.list.index_list[index];
+            Some((self.row, index))
+        } else {
+            None
+        }
+    }
+}
---
+++ src/sparsemat_rowvec.rs
@@ -0,0 +1,155 @@
+use crate::sparsemat::*;
+
+// A sparse matrix implementation where each row is stored in separate vec
+// This implementation makes at least two allocations each row
+#[derive(Clone, Debug)]
+pub struct SparseMatRowVec<T, I> {
+    n_cols: usize,
+    nnz: usize,
+    columns: Vec<Vec<I>>,
+    values: Vec<Vec<T>>,
+}
+ 
+impl<T, I> SparseMatRowVec<T, I>
+where T: ValueType,
+      I: IndexType {
+
+    // Returns the offset for the columns and values vec for row i
+    // or UNSET if entry (i, j) does not exist
+    fn find_index(&self, i: usize, j: usize) -> usize {
+        let col = I::as_indextype(j);
+        let mut ret = Self::UNSET.as_usize();
+        if i < self.n_rows() {
+            for index in 0..self.columns[i].len() {
+                if self.columns[i][index] == col {
+                    ret = index;
+                    break;
+                }
+            }
+        }
+        ret
+    }
+
+    fn push(&mut self, i: usize, j: usize, val: T) -> usize {
+        if i >= self.n_rows() {
+            self.columns.resize(i + 1, Vec::<I>::new());
+            self.values.resize(i + 1, Vec::<T>::new());
+        }
+        if j >= self.n_cols {
+            self.n_cols = j + 1;
+        }
+        let ret = self.columns[i].len();
+        self.columns[i].push(I::as_indextype(j));
+        self.values[i].push(val);
+        self.nnz += 1;
+        ret
+    }
+}
+
+impl<'a, T, I> SparseMat<'a> for SparseMatRowVec<T, I>
+where T: 'a + ValueType,
+      I: 'a + IndexType {
+
+    type Value = T;
+    type Index = I;
+    type Iter = Iter<'a, T, I>;
+    type IterRow = std::iter::Zip<std::slice::Iter<'a, I>, std::slice::Iter<'a, T>>;
+
+    fn iter(&'a self) -> Self::Iter {
+        Iter::<T, I> {
+            mat: self,
+            row: 0,
+            pos: 0,
+        }
+    }
+
+    fn iter_row(&'a self, row: usize) -> Self::IterRow {
+        if row < self.n_rows() {
+            self.columns[row].iter().zip(self.values[row].iter())
+        } else {
+            panic!("Invalid row {} - Max row is {}", row, self.n_rows());
+        }
+    }
+
+    fn new() -> Self {
+        Self {
+            n_cols: 0,
+            nnz: 0,
+            values: Vec::<Vec::<T>>::new(),
+            columns: Vec::<Vec::<I>>::new(),
+        }
+    }
+
+    fn with_capacity(cap: usize) -> Self {
+        Self {
+            n_cols: 0,
+            nnz: 0,
+            values: Vec::<Vec::<T>>::with_capacity(cap),
+            columns: Vec::<Vec::<I>>::with_capacity(cap),
+        }
+    }
+
+    fn n_rows(&self) -> usize {
+        self.columns.len()
+    }
+
+    fn n_cols(&self) -> usize {
+        self.n_cols
+    }
+    
+    fn n_non_zero_entries(&self) -> usize {
+        self.nnz
+    }
+
+    fn get(&self, i: usize, j: usize) -> T {
+        let mut ret = T::zero();
+        let index = self.find_index(i, j);
+        if index != Self::UNSET.as_usize() {
+            ret = self.values[i][index];
+        }
+        ret
+    }
+
+    fn get_mut(&mut self, i: usize, j: usize) -> &mut T {
+        let mut index = self.find_index(i, j);
+        if index == Self::UNSET.as_usize() {
+            index = self.push(i, j, T::zero());
+        }        
+        &mut self.values[i][index]
+    }
+
+    fn scale(&mut self, rhs: Self::Value) {
+        for i in 0..self.n_rows() {
+            for iter in self.values[i].iter_mut() {
+                *iter *= rhs;
+            }
+        }
+    }
+}
+
+pub struct Iter<'a, T, I> {
+    mat: &'a SparseMatRowVec<T, I>,
+    row: usize,
+    pos: usize,
+}
+
+impl<'a, T, I> Iterator for Iter<'a, T, I>
+where T: ValueType,
+      I: IndexType {
+    type Item = (usize, &'a I, &'a T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.row < (self.mat.n_rows() - 1)
+               && self.pos == self.mat.columns[self.row].len() {
+            self.row += 1;
+            self.pos = 0;
+        }
+        let index = self.pos;
+        if index < self.mat.columns[self.row].len() {
+            self.pos += 1;
+            Some((self.row, &self.mat.columns[self.row][index], &self.mat.values[self.row][index]))
+        } else {
+            None
+        }
+    }
+}
